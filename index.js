613868a1c141d790c1fb5c627359bd87e493fe01
felosa <fnandolopezsanchez@gmail.com>
2021-07-22 18:26:41 +0100

    add finished function, add tests

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-const groupArrayElements = (arr, n) => {
+const getArrayOfArraysWithSameLength = (data, n) => {
+  let result = [];
+  const numberOfElements = Math.floor(data.length / n);
+  for (let i = 0; i < n; i++) {
+    result.push(data.slice(0, numberOfElements));
+    data.splice(0, numberOfElements);
+  }
+  return result;
+};
 
-    let result = [];
-    const elemPerPart = Math.floor(arr.length / n);
-    for (let i = 0; i < n; i++) {
-        result.push(arr.slice(0, elemPerPart));
-        arr.splice(0, elemPerPart);
+const groupArrayElements = (arr, n, rest = []) => {
+
+  if (arr.length === 0 && rest.length === 0) {
+    return "The array can't be empty";
+  }
+
+  if (n <= 0 || isNaN(n)) {
+    return "This should be a positive number";
+  }
+
+  if(n > arr.length){
+      return "n can't be higher than the array length"
+  }
+
+  const arrayCopy = [...arr];
+  const isExact = arrayCopy.length % n === 0;
+
+  if (isExact) {
+    const result = getArrayOfArraysWithSameLength(arrayCopy, n);
+
+    if (rest.length > 0) {
+      result.push(rest);
     }
+
     return result;
+  } else {
+    if (rest.length > 0) {
+      rest.unshift(arrayCopy.pop());
+      return groupArrayElements(arrayCopy, n, rest);
+    } else {
+      rest.unshift(arrayCopy.pop());
+      return groupArrayElements(arrayCopy, n - 1, rest);
+    }
+  }
+};
 
-}
-
-
-console.log('Example: given the array [1, 2, 3, 4, 5, 6] divided in 3 parts: ', groupArrayElements([1, 2, 3, 4, 5, 6], 3))
+console.log(
+  "Example: given the array [1, 2, 3, 4, 5] divided in 3 parts: ",
+  groupArrayElements([1, 2, 3, 4, 5], 3)
+);
 
 module.exports = groupArrayElements;
--- index.test.js
+++ index.test.js
@@ -1,17 +1,32 @@
-const groupArrayElements = require('./index');
+const groupArrayElements = require("./index");
 
-test('returns error if the array is empty', () => {
-    const expected = "The array can't be empty"
-    expect(groupArrayElements([], 3)).toBe(expected);
+test("returns error if the array is empty", () => {
+  const expected = "The array can't be empty";
+  expect(groupArrayElements([], 3)).toBe(expected);
 });
 
-test('returns error if N is not a positive number', () => {
-    const expected = "The number should be positive"
-    expect(groupArrayElements([1, 2, 3, 4, 5, 6], -3)).toBe(expected);
+test("returns error if N is not a positive number", () => {
+  const expected = "This should be a positive number";
+  expect(groupArrayElements([1, 2, 3, 4, 5, 6], -3)).toBe(expected);
 });
 
-
-test('returns an array of arrays with the same length', () => {
-    expect(groupArrayElements([1, 2, 3, 4, 5, 6], 3)).toStrictEqual([[1, 2], [3, 4], [5, 6]]);
+test("returns error if N is higher than the array length", () => {
+  const expected = "n can't be higher than the array length";
+  expect(groupArrayElements([1, 2, 3, 4], 5)).toBe(expected);
 });
 
+test("returns an array of arrays with the same length", () => {
+  expect(groupArrayElements([1, 2, 3, 4, 5, 6], 3)).toStrictEqual([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ]);
+});
+
+test("returns an array of arrays with the last array having different length", () => {
+  expect(groupArrayElements([1, 2, 3, 4, 5, 6, 7], 3)).toStrictEqual([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7],
+  ]);
+});
